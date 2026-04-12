dba380cb841cab93cf79e7e7f397ebd7f945c53d
zack <zack@kali.zack>
2026-04-12 15:07:28 +0200

    Fix: Addressed all Structural and Security issues from Anatomical Audit

---
+++ server/run_audit.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const execSync = require('child_process').execSync;
+
+let files = execSync('find . -name "*.js" ! -path "*/node_modules/*" ! -path "*/vendor/*" ! -path "*/.git/*" ! -name "*.min.js*"').toString().split('\n').filter(Boolean);
+
+let backendFiles = files.filter(f => f.startsWith('./server/') || f === './dbsync.js' || f === './checkQuizzes.js');
+let frontendFiles = files.filter(f => f.startsWith('./client/'));
+
+console.log("Found " + backendFiles.length + " backend files and " + frontendFiles.length + " frontend files.");
--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,10 +1,11 @@
+const path = require('path');
 const winston = require('winston');
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.File({ filename: path.join(__dirname, '../error.log'), level: 'error' }),
+    new winston.transports.File({ filename: path.join(__dirname, '../combined.log') }),
     new winston.transports.Console({ format: winston.format.simple() })
   ]
 });
